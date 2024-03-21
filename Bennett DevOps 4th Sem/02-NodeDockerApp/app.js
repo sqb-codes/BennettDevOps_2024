4c91c8ff2cc429211d13184e66e858afcebfc9af
sqb-codes <150049400+sqb-codes@users.noreply.github.com>
2024-03-21 17:06:14 +0530

    Add files via upload

--- Bennett DevOps 4th Sem/02-NodeDockerApp/app.js
+++ Bennett DevOps 4th Sem/02-NodeDockerApp/app.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const {MONGO_IP, MONGO_PORT, MONGO_USER, MONGO_PASSWORD} = require("./config/config");
+
 const app = express();
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+mongoose.connect(
+    // "mongodb://root:root@192.168.112.2:27017/?authSource=admin")
+    // "mongodb://root:root@mongo:27017/?authSource=admin")
+    mongoURL)
+    .then(() => console.log("MongoDB started..."))
+    .catch((e) => {console.log("Error :",e)});
+
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello from Node JS</h1>");
+    res.send("<h1>Hello from Node JS with Docker...</h1>");
 });
 
 const PORT = process.env.PORT || 3000;
