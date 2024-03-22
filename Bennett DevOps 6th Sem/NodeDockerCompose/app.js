e7e45f08e1c9cfe0ca31a4359fe1f7d6b9803e58
sqb-codes <150049400+sqb-codes@users.noreply.github.com>
2024-03-22 10:22:10 +0530

    Add files via upload

--- Bennett DevOps 6th Sem/NodeDockerCompose/app.js
+++ Bennett DevOps 6th Sem/NodeDockerCompose/app.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
+
+mongoose.connect(
+    // "mongodb://root:root@172.28.0.2:27017/?authSource=admin")
+    "mongodb://root:root@mongo:27017/?authSource=admin")
+    .then(() => {
+        console.log("Mongo DB Started...");
+    })
+    .catch((err) => {
+        console.log("Error :",err)
+    });
+
 app.get("/", (req, res) => {
-    res.send("<h1>Hello world from Node with Docker Compose...</h1>");
+    res.send("<h1>Hello world from Node with Docker</h1>");
 });
 
 const PORT = process.env.PORT || 3000;
